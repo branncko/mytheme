303509571a66e9491b6578432d7fb25351172889
branncko <imprimarte@gmail.com>
2020-09-03 07:14:38 -0300

    sidebar

--- sidebar.php
+++ sidebar.php
@@ -5,29 +5,7 @@
     <?php 
     dynamic_sidebar( 'Cards' );
     ?>
-
-    <div class="card text-white mb-4">
-        <img src="./assets/image-1920x1080.jpg" class="card-img" alt="Descrição da Imagem">
-        <div class="card-img-overlay bg-banner-header p-0 m-0 row">
-            <div class="col align-self-end">
-                <h5 class="mb-0">Empréstimo Pessoal</h5>
-                <p>A solução para quem pensa no futuro</p>
-
-            </div>
-        </div>
-
-    </div>
-    <div class="card text-white mb-4">
-        <img src="./assets/image-1920x1080.jpg" class="card-img" alt="Descrição da Imagem">
-        <div class="card-img-overlay bg-banner-header p-0 m-0 row">
-            <div class="col align-self-end">
-                <h5 class="mb-0">Empréstimo Consignado</h5>
-                <p>A solução para quem pensa no futuro</p>
-
-            </div>
-        </div>
-
-    </div>
+    
     <div class="card bg-lc-gray text-center border-0">
         <div class="card-body">
             <ul class="list-unstyled list-inline mb-0">
