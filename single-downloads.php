869e2995c83fef3ef9cc2aa08f23c63dc961e2f5
branncko <imprimarte@gmail.com>
2020-09-25 14:57:25 -0300

    Banner Super rodape

--- single-downloads.php
+++ single-downloads.php
@@ -8,7 +8,7 @@
 
             <h1 class="mt-3 mb-2"><?php the_title(); ?></h1>
 
-             <?php the_post_thumbnail('mytheme-small', array('class' => 'center img-fluid mb-3') ); ?>
+            <?php the_post_thumbnail('mytheme-content', array('class' => 'center img-fluid mb-3') ); ?>
 
             <!-- <img src="<?php the_field('cmb2_down_imagem');?>" alt="imagem demonstrativa" class="img-fluid mb-3 p-2 card"> -->
 
@@ -60,37 +60,11 @@
 <div class="bg-lc-gray py-4 mt-4 mb-md-0">
     <div class="container">
         <div class="row">
+           
 
-            <?php 
+                <?php  dynamic_sidebar( 'Ads Topo' );  ?>
 
-                    $args = array('post_type' => 'depoimentos', 
-                    'posts_per_page' => 2
-                    );
-                    $the_query = new WP_Query( $args );
-                ?>
-
-            <?php if( $the_query -> have_posts() ) : 
-                while ( $the_query-> have_posts() ) : 
-                $the_query->the_post(); ?>
-
-            <div class="col-md-6 col-sm-12 mb-4 mb-md-0">
-                <div class="card border-card-footer">
-                    <div class="card-body">
-                        <?php the_content(); ?>
-                    </div>
-                </div>
-            </div>
-            <?php endwhile; ?>
-            <?php else : ?>
-            <div class="col-12">
-                <p class="lead">
-                    Nenhum Depoimento
-                </p>
-            </div>
-            <?php endif; ?>
-            <?php wp_reset_query(); ?>
-
-
+            
         </div>
     </div>
 </div>
