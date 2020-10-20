bdc8a08c8111b6e73346c97e876a353fa9b914e4
Branco Sousa <imprimarte@gmail.com>
2020-10-19 23:56:35 -0300

    avanços loop e archive

--- archive-downloads.php
+++ archive-downloads.php
@@ -1,8 +1,9 @@
 <!-- Adiciona o cabeçalho (header.php) -->
 <?php get_header(); ?>
-<div class="mh-wrapper clearfix">
-		<div class="mh-main clearfix">
-				<div id="main-content" class="mh-loop mh-content" role="main">
+<div class="container">
+		<div class="row">
+				<div class="col" role="main">
+					<p class="h1 text-primary"><?php echo get_the_title(); ?></p>
 					<?php 
 
 					get_template_part('loop', 'downloads'); 	
---
+++ category.php
@@ -0,0 +1,14 @@
+<?php get_header();?>
+<div class="container">
+
+    <div class="row my-4">
+
+        <?php get_template_part('loop', 'downloads'); ?>
+
+    </div>
+
+
+</div>
+
+
+<?php get_footer(); ?>
